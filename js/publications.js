2836274d9b1f01c81642ccd491c36f698c4f13e7
Gustavo Umbelino <gumbelin@ucsd.edu>
2018-06-24 00:05:33 -0400

    made it responsive

---
+++ js/publications.js
@@ -0,0 +1,14 @@
+const pubs = [
+  {
+    title: '',
+    authors: ['Chirstine Alvarado', 'Gustavo Umbelino', 'Mia Minnes'],
+    url: '',
+  },
+];
+
+new Vue({
+  el: '#pubs',
+  data: {
+    pubs,
+  },
+});
