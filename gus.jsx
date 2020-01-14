d4e26bbf5dc9eee7ffcb51618a155ac104018e82
Gustavo Umbelino <gumbelin@ucsd.edu>
2020-01-14 14:51:14 -0600

    added new file

---
+++ gus.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class gus extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+import React from 'react'
+
+export default function gus() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+import React, { Component } from 'react'
+
+export class gus extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default gus
